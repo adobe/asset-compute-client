8bafe99bf91758e214b804952a62db5b327f664b
dhendric <dhendric@adobe.com>
2020-06-25 14:14:27 -0600

    Fixed tests to be separate Added async/await where it was missing

--- test/integrationConfiguration.test.js
+++ test/integrationConfiguration.test.js
@@ -27,7 +27,7 @@ const privateKeyFile = path.join(process.cwd(), "test", "resources/privatekey.pe
 
 describe('integrationConfiguration.js tests', () => {
 
-    const expected = {
+    const expectedYaml = {
         metascopes: ['Meta Scope 1', 'Meta Scope 2', 'Meta Scope 3'],
         technicalAccount: {
             id: 'Tech Id',
@@ -38,180 +38,214 @@ describe('integrationConfiguration.js tests', () => {
         }
     };
 
-    it('Input file does not exist', async () => {
-
-        let actual;
-        const expected = new Error("Missing required files");
-
-        try {
-            await IntegrationConfiguration.getConfiguration("fake-file.file");
-        } catch (e) {
-            console.log(e);
-            actual = e;
+    const expectedJson = {
+        metascopes: ['Meta Scope 1', 'Meta Scope 2', 'Meta Scope 3'],
+        technicalAccount: {
+            id: 'Tech Id',
+            org: 'IMS Org Id',
+            clientId: 'Client Id',
+            clientSecret: 'Client Secret',
+            privateKey: 'PRIVATE KEY FILE\n'
         }
+    };
+    describe('YAML Input', () => {
 
-        assert.strictEqual(actual.name, expected.name);
-        assert.strictEqual(actual.message, expected.message);
+        it('Input file is YAML', async () => {
+            const actual = await IntegrationConfiguration.getConfiguration(yamlFile);
 
-        try {
-            await IntegrationConfiguration.getConfiguration(jsonFile, "fake-file.file");
-        } catch (e) {
-            console.log(e);
-            actual = e;
-        }
+            assert.strictEqual(actual.metascopes[0], expectedYaml.metascopes[0]);
+            assert.strictEqual(actual.metascopes[1], expectedYaml.metascopes[1]);
+            assert.strictEqual(actual.metascopes[2], expectedYaml.metascopes[2]);
 
-        assert.strictEqual(actual.name, expected.name);
-        assert.strictEqual(actual.message, expected.message);
+            assert.strictEqual(actual.technicalAccount.id, expectedYaml.technicalAccount.id);
+            assert.strictEqual(actual.technicalAccount.org, expectedYaml.technicalAccount.org);
+            assert.strictEqual(actual.technicalAccount.clientId, expectedYaml.technicalAccount.clientId);
+            assert.strictEqual(actual.technicalAccount.clientSecret, expectedYaml.technicalAccount.clientSecret);
+            assert.strictEqual(actual.technicalAccount.privateKey, expectedYaml.technicalAccount.privateKey);
+        });
 
-        try {
-            process.env.ASSET_COMPUTE_PRIVATE_KEY_FILE_PATH = "fake-file.file";
-            await IntegrationConfiguration.getConfiguration(jsonFile);
-        } catch (e) {
-            console.log(e);
-            actual = e;
-        }
+        it('Input file is YAML - ASSET_COMPUTE_INTEGRATION_FILE_PATH', async () => {
+            process.env.ASSET_COMPUTE_INTEGRATION_FILE_PATH = yamlFile;
+            const actual = await IntegrationConfiguration.getConfiguration();
 
-        assert.strictEqual(actual.name, expected.name);
-        assert.strictEqual(actual.message, expected.message);
+            assert.strictEqual(actual.metascopes[0], expectedYaml.metascopes[0]);
+            assert.strictEqual(actual.metascopes[1], expectedYaml.metascopes[1]);
+            assert.strictEqual(actual.metascopes[2], expectedYaml.metascopes[2]);
 
+            assert.strictEqual(actual.technicalAccount.id, expectedYaml.technicalAccount.id);
+            assert.strictEqual(actual.technicalAccount.org, expectedYaml.technicalAccount.org);
+            assert.strictEqual(actual.technicalAccount.clientId, expectedYaml.technicalAccount.clientId);
+            assert.strictEqual(actual.technicalAccount.clientSecret, expectedYaml.technicalAccount.clientSecret);
+            assert.strictEqual(actual.technicalAccount.privateKey, expectedYaml.technicalAccount.privateKey);
+        });
 
-        try {
+        it('Input file is missing properties', async () => {
+            let actual;
+            const yamlFile = path.join(process.cwd(), "test", "resources/integration-missing-properties.yaml");
+            const expected = new Error(`Not all integration configuration properties are present`);
+
+            try {
+                await IntegrationConfiguration.getConfiguration(yamlFile);
+            } catch (e) {
+                actual = e;
+            }
+
+            assert.strictEqual(actual.name, expected.name);
+            assert.strictEqual(actual.message, expected.message);
+        });
+
+        it('Input file is neither JSON nor YAML', async () => {
+            let actual;
+            const expected = new Error("Not all integration configuration properties are present");
+
+            try {
+                await IntegrationConfiguration.getConfiguration(plainTextFile);
+            } catch (e) {
+                console.log(e);
+                actual = e;
+            }
+
+            assert.strictEqual(actual.name, expected.name);
+            assert.strictEqual(actual.message, expected.message);
+        });
+
+        it('Input file does not exist', async () => {
+            let actual;
+            const expected = new Error("Missing required files");
+
+            try {
+                await IntegrationConfiguration.getConfiguration("fake-file.file");
+            } catch (e) {
+                console.log(e);
+                actual = e;
+            }
+
+            assert.strictEqual(actual.name, expected.name);
+            assert.strictEqual(actual.message, expected.message);
+        });
+
+        it('Input file does not exist - ASSET_COMPUTE_INTEGRATION_FILE_PATH', async () => {
+            let actual;
+            const expected = new Error("Missing required files");
             process.env.ASSET_COMPUTE_INTEGRATION_FILE_PATH = "fake-file.file";
-            await IntegrationConfiguration.getConfiguration();
-        } catch (e) {
-            console.log(e);
-            actual = e;
-        }
 
-        assert.strictEqual(actual.name, expected.name);
-        assert.strictEqual(actual.message, expected.message);
+            try {
+                await IntegrationConfiguration.getConfiguration();
+            } catch (e) {
+                console.log(e);
+                actual = e;
+            }
+
+            assert.strictEqual(actual.name, expected.name);
+            assert.strictEqual(actual.message, expected.message);
+        });
     });
 
-    it('Input file is neither JSON nor YAML', async () => {
+    describe('JSON Input', () => {
+        it('Input file is JSON', async () => {
+            const actual = await IntegrationConfiguration.getConfiguration(jsonFile, privateKeyFile);
 
-        let actual;
-        const expected = new Error("Not all integration configuration properties are present");
+            assert.strictEqual(actual.metascopes[0], expectedJson.metascopes[0]);
+            assert.strictEqual(actual.metascopes[1], expectedJson.metascopes[1]);
+            assert.strictEqual(actual.metascopes[2], expectedJson.metascopes[2]);
 
-        try {
-            await IntegrationConfiguration.getConfiguration(plainTextFile, privateKeyFile);
-        } catch (e) {
-            console.log(e);
-            actual = e;
-        }
+            assert.strictEqual(actual.technicalAccount.id, expectedJson.technicalAccount.id);
+            assert.strictEqual(actual.technicalAccount.org, expectedJson.technicalAccount.org);
+            assert.strictEqual(actual.technicalAccount.clientId, expectedJson.technicalAccount.clientId);
+            assert.strictEqual(actual.technicalAccount.clientSecret, expectedJson.technicalAccount.clientSecret);
+            assert.strictEqual(actual.technicalAccount.privateKey, expectedJson.technicalAccount.privateKey);
+        });
 
-        assert.strictEqual(actual.name, expected.name);
-        assert.strictEqual(actual.message, expected.message);
+        it('Input file is JSON - ASSET_COMPUTE_INTEGRATION_FILE_PATH & ASSET_COMPUTE_PRIVATE_KEY_FILE_PATH', async () => {
+            process.env.ASSET_COMPUTE_INTEGRATION_FILE_PATH = jsonFile;
+            process.env.ASSET_COMPUTE_PRIVATE_KEY_FILE_PATH = privateKeyFile;
+            const actual = await IntegrationConfiguration.getConfiguration();
 
-        try {
-            await IntegrationConfiguration.getConfiguration(plainTextFile);
-        } catch (e) {
-            console.log(e);
-            actual = e;
-        }
+            assert.strictEqual(actual.metascopes[0], expectedJson.metascopes[0]);
+            assert.strictEqual(actual.metascopes[1], expectedJson.metascopes[1]);
+            assert.strictEqual(actual.metascopes[2], expectedJson.metascopes[2]);
 
-        assert.strictEqual(actual.name, expected.name);
-        assert.strictEqual(actual.message, expected.message);
-    });
+            assert.strictEqual(actual.technicalAccount.id, expectedJson.technicalAccount.id);
+            assert.strictEqual(actual.technicalAccount.org, expectedJson.technicalAccount.org);
+            assert.strictEqual(actual.technicalAccount.clientId, expectedJson.technicalAccount.clientId);
+            assert.strictEqual(actual.technicalAccount.clientSecret, expectedJson.technicalAccount.clientSecret);
+            assert.strictEqual(actual.technicalAccount.privateKey, expectedJson.technicalAccount.privateKey);
+        });
 
-    it('Input file is YAML', async () => {
+        it('Input file is JSON - ASSET_COMPUTE_PRIVATE_KEY_FILE_PATH', async () => {
+            process.env.ASSET_COMPUTE_PRIVATE_KEY_FILE_PATH = privateKeyFile;
+            const actual = await IntegrationConfiguration.getConfiguration(jsonFile);
 
-        let actual = await IntegrationConfiguration.getConfiguration(yamlFile);
+            assert.strictEqual(actual.metascopes[0], expectedJson.metascopes[0]);
+            assert.strictEqual(actual.metascopes[1], expectedJson.metascopes[1]);
+            assert.strictEqual(actual.metascopes[2], expectedJson.metascopes[2]);
 
-        assert.strictEqual(actual.metascopes[0], expected.metascopes[0]);
-        assert.strictEqual(actual.metascopes[1], expected.metascopes[1]);
-        assert.strictEqual(actual.metascopes[2], expected.metascopes[2]);
+            assert.strictEqual(actual.technicalAccount.id, expectedJson.technicalAccount.id);
+            assert.strictEqual(actual.technicalAccount.org, expectedJson.technicalAccount.org);
+            assert.strictEqual(actual.technicalAccount.clientId, expectedJson.technicalAccount.clientId);
+            assert.strictEqual(actual.technicalAccount.clientSecret, expectedJson.technicalAccount.clientSecret);
+            assert.strictEqual(actual.technicalAccount.privateKey, expectedJson.technicalAccount.privateKey);
+        });
 
-        assert.strictEqual(actual.technicalAccount.id, expected.technicalAccount.id);
-        assert.strictEqual(actual.technicalAccount.org, expected.technicalAccount.org);
-        assert.strictEqual(actual.technicalAccount.clientId, expected.technicalAccount.clientId);
-        assert.strictEqual(actual.technicalAccount.clientSecret, expected.technicalAccount.clientSecret);
-        assert.strictEqual(actual.technicalAccount.privateKey, expected.technicalAccount.privateKey);
+        it('Input file is missing properties', async () => {
+            let actual;
+            const jsonFile = path.join(process.cwd(), "test", "resources/integration-missing-properties.json");
+            const expected = new Error(`Not all integration configuration properties are present`);
 
+            try {
+                await IntegrationConfiguration.getConfiguration(jsonFile, privateKeyFile);
+            } catch (e) {
+                actual = e;
+            }
 
-        process.env.ASSET_COMPUTE_INTEGRATION_FILE_PATH = yamlFile;
-        actual = await IntegrationConfiguration.getConfiguration();
+            assert.strictEqual(actual.name, expected.name);
+            assert.strictEqual(actual.message, expected.message);
+        });
 
-        assert.strictEqual(actual.metascopes[0], expected.metascopes[0]);
-        assert.strictEqual(actual.metascopes[1], expected.metascopes[1]);
-        assert.strictEqual(actual.metascopes[2], expected.metascopes[2]);
+        it('Input file is neither JSON nor YAML', async () => {
+            let actual;
+            const expected = new Error("Not all integration configuration properties are present");
 
-        assert.strictEqual(actual.technicalAccount.id, expected.technicalAccount.id);
-        assert.strictEqual(actual.technicalAccount.org, expected.technicalAccount.org);
-        assert.strictEqual(actual.technicalAccount.clientId, expected.technicalAccount.clientId);
-        assert.strictEqual(actual.technicalAccount.clientSecret, expected.technicalAccount.clientSecret);
-        assert.strictEqual(actual.technicalAccount.privateKey, expected.technicalAccount.privateKey);
-    });
+            try {
+                await IntegrationConfiguration.getConfiguration(plainTextFile, privateKeyFile);
+            } catch (e) {
+                console.log(e);
+                actual = e;
+            }
 
-    it('Input file is JSON', async () => {
+            assert.strictEqual(actual.name, expected.name);
+            assert.strictEqual(actual.message, expected.message);
+        });
 
-        expected.technicalAccount.privateKey = "PRIVATE KEY FILE\n";
-        let actual = await IntegrationConfiguration.getConfiguration(jsonFile, privateKeyFile);
+        it('Private key file does not exist', async () => {
+            let actual;
+            const expected = new Error("Missing required files");
 
-        assert.strictEqual(actual.metascopes[0], expected.metascopes[0]);
-        assert.strictEqual(actual.metascopes[1], expected.metascopes[1]);
-        assert.strictEqual(actual.metascopes[2], expected.metascopes[2]);
+            try {
+                await IntegrationConfiguration.getConfiguration(jsonFile, "fake-file.file");
+            } catch (e) {
+                console.log(e);
+                actual = e;
+            }
 
-        assert.strictEqual(actual.technicalAccount.id, expected.technicalAccount.id);
-        assert.strictEqual(actual.technicalAccount.org, expected.technicalAccount.org);
-        assert.strictEqual(actual.technicalAccount.clientId, expected.technicalAccount.clientId);
-        assert.strictEqual(actual.technicalAccount.clientSecret, expected.technicalAccount.clientSecret);
-        assert.strictEqual(actual.technicalAccount.privateKey, expected.technicalAccount.privateKey);
+            assert.strictEqual(actual.name, expected.name);
+            assert.strictEqual(actual.message, expected.message);
+        });
 
+        it('Private key file does not exist - ASSET_COMPUTE_PRIVATE_KEY_FILE_PATH', async () => {
+            let actual;
+            const expected = new Error("Missing required files");
+            process.env.ASSET_COMPUTE_PRIVATE_KEY_FILE_PATH = "fake-file.file";
 
-        process.env.ASSET_COMPUTE_INTEGRATION_FILE_PATH = jsonFile;
-        process.env.ASSET_COMPUTE_PRIVATE_KEY_FILE_PATH = privateKeyFile;
-        actual = await IntegrationConfiguration.getConfiguration();
+            try {
+                await IntegrationConfiguration.getConfiguration(jsonFile);
+            } catch (e) {
+                console.log(e);
+                actual = e;
+            }
 
-        assert.strictEqual(actual.metascopes[0], expected.metascopes[0]);
-        assert.strictEqual(actual.metascopes[1], expected.metascopes[1]);
-        assert.strictEqual(actual.metascopes[2], expected.metascopes[2]);
-
-        assert.strictEqual(actual.technicalAccount.id, expected.technicalAccount.id);
-        assert.strictEqual(actual.technicalAccount.org, expected.technicalAccount.org);
-        assert.strictEqual(actual.technicalAccount.clientId, expected.technicalAccount.clientId);
-        assert.strictEqual(actual.technicalAccount.clientSecret, expected.technicalAccount.clientSecret);
-        assert.strictEqual(actual.technicalAccount.privateKey, expected.technicalAccount.privateKey);
-
-
-        process.env.ASSET_COMPUTE_PRIVATE_KEY_FILE_PATH = privateKeyFile;
-        actual = await IntegrationConfiguration.getConfiguration(jsonFile);
-
-        assert.strictEqual(actual.metascopes[0], expected.metascopes[0]);
-        assert.strictEqual(actual.metascopes[1], expected.metascopes[1]);
-        assert.strictEqual(actual.metascopes[2], expected.metascopes[2]);
-
-        assert.strictEqual(actual.technicalAccount.id, expected.technicalAccount.id);
-        assert.strictEqual(actual.technicalAccount.org, expected.technicalAccount.org);
-        assert.strictEqual(actual.technicalAccount.clientId, expected.technicalAccount.clientId);
-        assert.strictEqual(actual.technicalAccount.clientSecret, expected.technicalAccount.clientSecret);
-        assert.strictEqual(actual.technicalAccount.privateKey, expected.technicalAccount.privateKey);
-    });
-
-    it('Input file is missing properties', async () => {
-
-        const yamlFile = path.join(process.cwd(), "test", "resources/integration-missing-properties.yaml");
-        const jsonFile = path.join(process.cwd(), "test", "resources/integration-missing-properties.json");
-
-
-        let actual;
-        const expected = new Error(`Not all integration configuration properties are present`);
-
-        try {
-            await IntegrationConfiguration.getConfiguration(jsonFile, privateKeyFile);
-        } catch (e) {
-            actual = e;
-        }
-
-        assert.strictEqual(actual.name, expected.name);
-        assert.strictEqual(actual.message, expected.message);
-
-        try {
-            await IntegrationConfiguration.getConfiguration(yamlFile);
-        } catch (e) {
-            actual = e;
-        }
-
-        assert.strictEqual(actual.name, expected.name);
-        assert.strictEqual(actual.message, expected.message);
+            assert.strictEqual(actual.name, expected.name);
+            assert.strictEqual(actual.message, expected.message);
+        });
     });
 });
